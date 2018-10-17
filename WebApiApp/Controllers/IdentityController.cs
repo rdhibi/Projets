37b6000276df801d04eab08c48af6ec9dcd83c23
Dhibi Radhouan <dradhouan@vdm.com>
2018-10-17 18:36:32 +0200

    first commit

---
+++ WebApiApp/Controllers/IdentityController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiApp.Helpers;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    /// <summary>
+    /// classe d'identification
+    /// </summary>
+    public class IdentityController : ApiController
+    {
+        #region Api
+
+        /// <summary>
+        /// authentication via email et passwort
+        /// </summary>
+        /// <param name="email">email</param>
+        /// <param name="password">mot de passe</param>
+        /// <returns></returns>
+        [Route("api/authenticate/{email}/{password}")]
+        [System.Web.Http.HttpGet]
+        public bool Authenticate(string email, string password)
+        {
+            if (!Users.Any()) return false;
+
+            var userList = Users.Where(u => Login(u, email, password)).ToList();
+
+            return userList != null && userList.Any();
+        }
+
+
+        /// <summary>
+        /// vérif accès non sécurisé
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [Route("api/confidentials/{email}")]
+        [System.Web.Http.HttpGet]
+        public bool Confidentials(string email)
+        {
+            return GetConfidentials(email);
+        }
+
+        /// <summary>
+        /// vérif accès avec autorisation
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>retourne si accès autorisé</returns>
+        [AuthorizationAttribute]
+        [Route("api/ConfidentialsWithAuthorization/{email}")]
+        [System.Web.Http.HttpGet]
+        public bool ConfidentialsWithAuthorization(string email)
+        {
+            return GetConfidentials(email);
+        }
+
+        /// <summary>
+        /// vérif accès
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        private bool GetConfidentials(string email)
+        {
+            if (!UsersLoged.Any()) return false;
+
+            var userList = UsersLoged.Where(u => Loged(u, email, "")).ToList();
+
+            return userList != null && userList.Any();
+        }
+
+        #endregion
+
+        #region acess
+        /// <summary>
+        /// données
+        /// </summary>
+        User[] Users = new User[]
+        {
+            new User { Email = "user1@test.com", Password = "user1" },
+            new User { Email = "user2@test.com", Password = "user2" },
+            new User { Email = "user3@test.com", Password = "user3" },
+        };
+
+        User[] UsersLoged = new User[]
+        {
+            new User { Email = "user1@test.com", Password = "user1" }
+        };
+
+        /// <summary>
+        /// déligué pour créer des filtres
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public delegate bool UserLogin(User u, string email, string password);
+        public static UserLogin Login = (u, email, password) =>
+        {
+            return u != null && !string.IsNullOrEmpty(u.Email) && !string.IsNullOrEmpty(u.Password)
+                && u.Email.Equals(email) && u.Password.Equals(password); ;
+        };
+        public static UserLogin Loged = (myUser, email, password) =>
+        {
+            return myUser != null && !string.IsNullOrEmpty(myUser.Email)
+                && myUser.Email.Equals(email);
+        };
+        #endregion
+
+    }
+}
---
+++ WebApiApp/Helpers/AuthorizationAttribute.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiApp.Helpers
+{
+    /// <summary>
+    /// attribut d'autorisation
+    /// </summary>
+    public class AuthorizationAttribute : AuthorizeAttribute
+    {
+
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+
+            if (AuthorizeRequest(actionContext))
+            {
+
+                return;
+
+            }
+
+            HandleUnauthorizedRequest(actionContext);
+
+        }
+
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+
+            actionContext.Response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Forbidden,
+                Content = new StringContent("You are unauthorized to access this resource")
+            };
+
+        }
+
+        private bool AuthorizeRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            //signature user 1 mise sur coté client jquery pour le test
+            //string sign1 = CreateHmac("user1@test.com");
+
+            string Authorization = actionContext.Request.Headers.Authorization != null ? 
+                actionContext.Request.Headers.Authorization.ToString().Replace("Basic ","") : "";
+
+            string mail = "", signature = "";
+
+            if (!Authorization.Contains(' ')) return false;
+
+            mail = Authorization.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+
+            signature = Authorization.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
+
+            if (string.IsNullOrEmpty(mail) || string.IsNullOrEmpty(signature)) return false;
+
+            string signatureDB = CreateHmac(mail);
+
+            return signatureDB == signature;
+            
+
+        }
+
+        /// <summary>
+        /// fonction qui doit être dans une classe de hashage(helper)
+        /// </summary>
+        /// <param name="toHash"></param>
+        /// <returns></returns>
+        private string CreateHmac( string toHash)
+        {
+
+            string hashPasswordHexa = "9ba1f63365a6caf66e46348f43cdef956015bea997adeb06e69007ee3ff517df10fc5eb860da3d43b82c2a040c931119d2dfc6d08e253742293a868cc2d82015"; 
+            byte[] hashPassword = ConvertHexStringToByteArray(hashPasswordHexa);
+
+            string hashString;
+
+            using (var hmac = new HMACSHA512(hashPassword))
+            {
+                var hash = hmac.ComputeHash(Encoding.ASCII.GetBytes(toHash));
+                hashString = BitConverter.ToString(hash).Replace("-", string.Empty).ToUpper();
+            }
+            return hashString;
+
+        }
+
+        private static byte[] ConvertHexStringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+    }
+}
